1d5dc68a824b91a275251a7456ccc90684fa224f
Tim Seemann <t.l.seemann@gmx.de>
2022-06-20 19:52:43 +0200

    Spiel mit einzelnen Lines

--- Spiele/Copyolo/spiel.php
+++ Spiele/Copyolo/spiel.php
@@ -6,6 +6,32 @@ $elemente = array();
 while (($line = fgetcsv($handle)) !== FALSE) {
     array_push($elemente,$line);
 }
-echo implode(', ', array_values($elemente[rand(0,count($elemente)-1)]));
 fclose($handle);
 ?>
+<html lang="de">
+<head>
+    <link rel="icon" href="../../Grafiken/icon.jpg">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Links zu den Unterseiten</title>
+</head>
+<body>
+<p><a id="ausgabe">Jetzt gehts los!!!</a></p>
+<p><a><button onclick="weiter()">weiter</a></p>
+<script>
+    let elemente = <?php echo json_encode($elemente); ?>;
+    const ausgabe = document.getElementById("ausgabe");
+    function weiter(){
+        if(elemente.length!==0) {
+            var ran = Math.floor(Math.random() * (elemente.length));
+            ausgabe.textContent = elemente[ran];
+            elemente.splice(ran, 1);
+        }
+        else{
+            ausgabe.textContent="Das Spiel ist vorbei :)";
+        }
+    }
+</script>
+</body>
+</html>
+
